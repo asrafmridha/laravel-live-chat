5326e02a85732730e1e6fdd88857c07c2acd3b51
asraf-mridha <asrafmridha@gmail.com>
2023-02-25 19:32:58 +0600

    Design and Ajax Setup for user interface

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,50 +1,55 @@
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Live Chat Application</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  </head>
-  <body>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    @vite(['resources/css/app.css' , 'resources/js/app.js'])
+    <title>Live Chat</title>
+</head>
+<body>
+<div class="app">
+    <div class="row">
 
-    <div class="app">
-        <div class="row">
-            <div class="col-sm-6 offset-sm-3 my-2">
-                <input type="text" class="form-control" name="username" id="username" placeholder="Enter a user name........">
-            </div>
-            <div class="col-sm-6 offset-sm-3">
-                <div class="box box-primary direct-chat direct-chat-primary">
-                    <div class="box-body">
-                        <div class="direct-chat-message" id="message"></div>
-                    </div>
-                    <div class="box-footer">
-                        <form action="">
-                            <div class="input-group">
-                                <input type="text" name="message" id="message" class="form-control" placeholder="Enter a message.....">
-                                <span class="input-group-btn">
-                                    <button class="btn btn-primary btn-flat" type="submit" id="send_message">Send</button>
-                                </span>
-                            </div>
-                        </form>
-                    </div>
+        <div class="col-sm-6 offset-sm-3 my-2">
+            <input type="text" class="form-control" name="username" id="username" placeholder="Enter a user ..........">
+        </div>
+
+        <div class="col-sm-6 offset-sm-3">
+            <div class="box box-primary direct-chat direct-chat-primary">
+
+                <div class="box-body">
+                    <div class="direct-chat-messages" id="messages"></div>
                 </div>
+
+                <div class="box-footer">
+                    <form action="#" method="post" id="message_form">
+                        <div class="input-group">
+                            <input type="text" name="message" id="message" placeholder="Type Message ..." class="form-control">
+                            <span class="input-group-btn">
+                                <button type="submit" id="send_message"  class="btn btn-primary btn-flat">Send</button>
+                          </span>
+                        </div>
+                    </form>
+                </div>
+
             </div>
         </div>
+
     </div>
+</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-    <script>
-        $.ajaxSetup({
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src ="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
+    $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-
-    </script>
-  </body>
+</script>
+</body>
 </html>
